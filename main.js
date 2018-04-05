de70561af102b2a47b306f523b0d18a866fef02c
Aleksey Chuyan <a.chuyan@videal.net>
2018-04-05 23:57:51 +0300

    add changes

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "30%";
+  document.getElementById("main-sidebar").style.width = "30%";
 }
 
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main-sidebar").style.width = "0";
 }
 
+/**
+ * Render new image in main-graphic element according to image num parameter
+ * @param num - image number. All images should be named as num.jpg and be placed at static/images folder
+ */
+function changeGraphic(num) {
+  // TODO delete previous image before append next child
+
+  var graphic = document.getElementById('main-graphic');
+  var img = document.createElement('img');
+  img.setAttribute('src', 'static/images/'+num+'.jpg');
+  img.setAttribute('alt', 'main graphic');
+
+  graphic.appendChild(img)
+}
+
+window.onload = function() {
+  changeGraphic(1)
+};
 
